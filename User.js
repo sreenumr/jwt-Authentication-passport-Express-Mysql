fe74e08fe23dbe831e71e5a6a011152d5c9be2bc
sreenumr <sreenu959@gmail.com>
2019-07-01 17:20:39 +0530

    User module

---
+++ User.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var app = express();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("sys", "sreenu", "password", {
+  dialect: "mysql",
+  port: "3306",
+  host: "127.0.0.1"
+});
+
+class Users extends Sequelize.Model {}
+Users.init(
+  {
+    username: {
+      type: Sequelize.STRING,
+      primaryKey: true
+    },
+    password: Sequelize.STRING
+  },
+  { sequelize, modelName: "Users", timestamps: false }
+);
+
+const createUser = ({ username, password }) => {
+  Users.build({
+    username,
+    password
+  })
+    .save()
+    .then(() => {
+      console.log("Data saved");
+    })
+    .catch(e => {
+      console.log(e);
+    });
+};
+
+module.exports = createUser;
--- index.js
+++ index.js
@@ -3,49 +3,18 @@ var app = express();
 var jwt = require("jsonwebtoken");
 var passport = require("passport");
 var bodyParser = require("body-parser");
+var createUser = require("./User");
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("sys", "sreenu", "password", {
-  dialect: "mysql",
-  port: "3306",
-  host: "127.0.0.1"
-});
+const user = {
+  username: "james bond",
+  password: "password"
+};
+
+createUser(user);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
-class Users extends Sequelize.Model {}
-Users.init(
-  {
-    username: {
-      type: Sequelize.STRING,
-      primaryKey: true
-    },
-    password: Sequelize.STRING
-  },
-  { sequelize, modelName: "Users", timestamps: false }
-);
-
-// sequelize.define("Users", {
-//   username: {
-//     type: Sequelize.STRING,
-//     primaryKey: true
-//   },
-//   password: Sequelize.STRING
-// });
-
-Users.build({
-  username: "Jane Doe",
-  password: "password"
-})
-  .save()
-  .then(() => {
-    console.log("Data saved");
-  })
-  .catch(e => {
-    console.log(e);
-  });
 
 // app.post("/auth", (req, res) => {
 //   res.json({
